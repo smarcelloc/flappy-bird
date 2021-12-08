de0b3f934ea12647f2ba7e02d5ae0c5e667d65be
Marcello Silvério <smarcelloc@gmail.com>
2021-12-07 22:29:53 -0300

    feat: add support file extensions of image and audio in project

---
+++ src/app.env.d.ts
@@ -0,0 +1,51 @@
+// Declare image file
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+// declare module '*.jpg' {
+//   const src: string;
+//   export default src;
+// }
+
+// declare module '*.jpeg' {
+//   const src: string;
+//   export default src;
+// }
+
+// declare module '*.svg' {
+//   const src: string;
+//   export default src;
+// }
+
+// declare module '*.gif' {
+//   const src: string;
+//   export default src;
+// }
+
+// Declare audio file
+declare module '*.wav' {
+  const src: string;
+  export default src;
+}
+
+// declare module '*.mp3' {
+//   const src: string;
+//   export default src;
+// }
+
+// declare module '*.ogg' {
+//   const src: string;
+//   export default src;
+// }
+
+// declare module '*.aac' {
+//   const src: string;
+//   export default src;
+// }
+
+// declare module '*.wma' {
+//   const src: string;
+//   export default src;
+// }
