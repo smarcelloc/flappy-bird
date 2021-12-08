f2e3a715c30d4e237521e3a57c5c5ed07847f378
Marcello Silvério <smarcelloc@gmail.com>
2021-12-07 23:33:13 -0300

    feat: draw of flappy bird

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('ok');
+import spritesImage from './assets/sprites.png';
+
+const sprites = new Image();
+sprites.src = spritesImage;
+
+const canvas: HTMLCanvasElement | null = document.querySelector('canvas#root');
+const context = canvas?.getContext('2d');
+
+function loop() {
+  context?.drawImage(sprites, 0, 0, 35, 24, 25, 50, 35, 24);
+  requestAnimationFrame(loop);
+}
+
+loop();
