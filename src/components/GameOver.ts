1b93bb14587805ec8dcae1a0ad5336b45f99e5a4
Marcello Silvério <smarcelloc@gmail.com>
2021-12-08 02:16:57 -0300

    feat: create component GameOver

---
+++ src/components/GameOver.ts
@@ -0,0 +1,29 @@
+import DrawImage from '@src/interfaces/DrawImage';
+import { canvas, context, sprites } from '@src/util/sprites';
+
+class GameOver implements DrawImage {
+  image = sprites;
+  spriteX = 133;
+  spriteY = 152;
+  spriteWidth = 228;
+  spriteHeight = 200;
+  canvasWidth = this.spriteWidth;
+  canvasHeight = this.spriteHeight;
+  canvasX = Math.floor(canvas.width / 2 - this.canvasWidth / 2);
+  canvasY = 50;
+  show(): void {
+    context.drawImage(
+      this.image,
+      this.spriteX,
+      this.spriteY,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.canvasX,
+      this.canvasY,
+      this.canvasWidth,
+      this.canvasHeight
+    );
+  }
+}
+
+export default GameOver;
